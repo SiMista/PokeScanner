9bf95dabb88793e485df129ddae09f92aebbfb3c
SiMista <simeondeiva@gmail.com>
2024-04-25 22:37:09 +0200

    Collection section with new logo

---
+++ app/src/main/java/com/example/pokescanner/ui/collection/CollectionFragment.java
@@ -0,0 +1,37 @@
+package com.example.pokescanner.ui.collection;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.pokescanner.databinding.FragmentCollectionBinding;
+
+public class CollectionFragment extends Fragment {
+
+    private FragmentCollectionBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CollectionViewModel collectionViewModel =
+                new ViewModelProvider(this).get(CollectionViewModel.class);
+
+        binding = FragmentCollectionBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textCollection;
+        collectionViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
--- app/src/main/java/com/example/pokescanner/ui/collection/CollectionViewModel.java
+++ app/src/main/java/com/example/pokescanner/ui/collection/CollectionViewModel.java
@@ -1,16 +1,16 @@
-package com.example.pokescanner.ui.notifications;
+package com.example.pokescanner.ui.collection;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class CollectionViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public CollectionViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is collection fragment");
     }
 
     public LiveData<String> getText() {
